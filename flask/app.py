fd4611063e1378085d63bccc749e1ea8ca205718
Saad <s3717159@student.rmit.edu.au>
2020-07-05 12:16:54 +1000

    mock data created -- excluding latitude and longitude need to discuss with team

--- flask/app.py
+++ flask/app.py
@@ -4,6 +4,23 @@ app = Flask(__name__)
 
 app.secret_key="webooster"
 
+mockData = [
+    {'time' : 1, 'temperature' : 0, 'altitude': 0, 'acceleration': 0 , 'latitude' : 0 , 'longitude' : 0},
+    {'time' : 2, 'temperature' : 10, 'altitude': 100, 'acceleration': 10 , 'latitude' : 0 , 'longitude' : 0},
+    {'time' : 3, 'temperature' : 20, 'altitude': 200, 'acceleration': 20 , 'latitude' : 0 , 'longitude' : 0},
+    {'time' : 4, 'temperature' : 30, 'altitude': 300, 'acceleration': 30 , 'latitude' : 0 , 'longitude' : 0},
+    {'time' : 5, 'temperature' : 40, 'altitude': 400, 'acceleration': 40 , 'latitude' : 0 , 'longitude' : 0},
+    {'time' : 6, 'temperature' : 50, 'altitude': 500, 'acceleration': 50 , 'latitude' : 0 , 'longitude' : 0},
+    {'time' : 7, 'temperature' : 60, 'altitude': 600, 'acceleration': 50 , 'latitude' : 0 , 'longitude' : 0},
+    {'time' : 8, 'temperature' : 60, 'altitude': 700, 'acceleration': 60 , 'latitude' : 0 , 'longitude' : 0},
+    {'time' : 9, 'temperature' : 55, 'altitude': 800, 'acceleration': 70 , 'latitude' : 0 , 'longitude' : 0},
+    {'time' : 10, 'temperature' : 50, 'altitude': 900, 'acceleration': 80 , 'latitude' : 0 , 'longitude' : 0},
+    {'time' : 11, 'temperature' : 40, 'altitude': 750, 'acceleration': 70 , 'latitude' : 0 , 'longitude' : 0},
+    {'time' : 12, 'temperature' : 30, 'altitude': 300, 'acceleration': 60 , 'latitude' : 0 , 'longitude' : 0},
+    {'time' : 13, 'temperature' : 20, 'altitude': 200, 'acceleration': 50 , 'latitude' : 0 , 'longitude' : 0},
+    {'time' : 14, 'temperature' : 5, 'altitude': 100, 'acceleration': 20 , 'latitude' : 0 , 'longitude' : 0},
+
+]
 
 @app.route('/main')
 def mainPage():
