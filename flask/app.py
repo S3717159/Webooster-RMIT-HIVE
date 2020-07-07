08ceb6fd50d3bc4dbe0687847a2505b0b62a4191
Saad <s3717159@student.rmit.edu.au>
2020-07-07 11:36:09 +1000

    back end working smoothly

--- flask/app.py
+++ flask/app.py
@@ -2,12 +2,14 @@ from flask import Flask
 from flask import Flask , redirect , url_for, render_template , request, session, flash, jsonify,make_response
 import json
 import random
+import time
+
 app = Flask(__name__)
 
 app.secret_key="webooster"
 
 mockData = [
-    {'time' : 1, 'temperature' : 0, 'altitude': 0, 'acceleration': 0 , 'latitude' : 0 , 'longitude' : 0},
+    {'time' : 1, 'temperature' : 1, 'altitude': 1, 'acceleration': 1 , 'latitude' : 0 , 'longitude' : 0},
     {'time' : 2, 'temperature' : 10, 'altitude': 100, 'acceleration': 10 , 'latitude' : 0 , 'longitude' : 0},
     {'time' : 3, 'temperature' : 20, 'altitude': 200, 'acceleration': 20 , 'latitude' : 0 , 'longitude' : 0},
     {'time' : 4, 'temperature' : 30, 'altitude': 300, 'acceleration': 30 , 'latitude' : 0 , 'longitude' : 0},
@@ -23,6 +25,11 @@ mockData = [
     {'time' : 14, 'temperature' : 5, 'altitude': 100, 'acceleration': 20 , 'latitude' : 0 , 'longitude' : 0},
 
 ]
+currentIndex = 0
+previousData = {'time' : 1, 'temperature' : 1, 'altitude': 1, 'acceleration': 1 , 'latitude' : 0 , 'longitude' : 0}
+currentData = mockData[currentIndex]
+dataChange = {'temperatureChange' : 1, 'altitudeChange': 1, 'accelerationChange': 1 }
+
 
 @app.route('/main')
 def mainPage():
@@ -34,15 +41,59 @@ def boot():
 
 @app.route('/data', methods=["GET", "POST"])
 def data():
-    x = random.randint(0,13)
-    print(x)
-    data = mockData[x]
+    global dataChange
+    global currentData
+    data = Merge(dataChange, currentData)
     response = make_response(json.dumps(data))
 
     response.content_type = 'application/json'
 
     return response
 
+@app.route('/updater', methods=["GET", "POST"])
+def updater():
+    while True:
+        print('updating' )
+        global currentData
+        global currentIndex
+        global previousData
+        if currentIndex == 14:
+            currentIndex = 0
+        dataChangeUpdate()
+        previousData = currentData
+        currentData = mockData[currentIndex]
+        currentIndex = currentIndex + 1
+        time.sleep(1)
+    
+   
+    
+    
+
 app.config['TEMPLATES_AUTO_RELOAD'] = True
 if app == "__main__":
-    app.run()
+    app.run(threaded=True)
+
+
+def dataChangeUpdate():
+    global currentData
+    global previousData
+    global dataChange
+
+    temperatureChange = calculateChange(previousData['temperature'],currentData['temperature'] )
+    altitudeChange = calculateChange(previousData['altitude'],currentData['altitude'] )
+    accelerationChange = calculateChange(previousData['acceleration'],currentData['acceleration'] )
+    
+    dataChange['temperatureChange'] = temperatureChange
+    dataChange['altitudeChange'] = altitudeChange
+    dataChange['accelerationChange'] = accelerationChange
+
+def calculateChange(new, old):
+    
+    change = ((old - new )/old) * 100
+    
+    return round(change, 2)
+
+
+def Merge(dict1, dict2): 
+    res = {**dict1, **dict2} 
+    return res 
